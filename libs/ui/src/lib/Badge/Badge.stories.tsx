2b80f767a56bbdbd51481905d26927a38e1191e8
John Ardosa <jardosa@live.com>
2023-09-29 08:44:38 +0800

    feat: add Badge component

--- libs/ui/src/lib/Badge/Badge.stories.tsx
+++ libs/ui/src/lib/Badge/Badge.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import Badge from './Badge';
+import Badge, { BadgeProps } from './Badge';
+import { CalendarIcon } from '@heroicons/react/24/solid'
 
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
@@ -7,6 +8,12 @@ import { expect } from '@storybook/jest';
 const meta: Meta<typeof Badge> = {
   component: Badge,
   title: 'Badge',
+  argTypes: {
+    intent: {
+      options: ['neutral-muted', 'neutral', 'info', 'danger', 'success', 'warning', 'tier'] as Array<BadgeProps['intent']>,
+      control: { type: 'radio' },
+    }
+  }
 };
 export default meta;
 type Story = StoryObj<typeof Badge>;
@@ -15,10 +22,7 @@ export const Primary: Story = {
   args: {
     label: "Pipeline badge",
     size: "SM",
-    icon: <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="8" cy="8" r="7.5" fill="white" stroke="#666666" />
-      <path d="M12 8.7205V7.2795L11.1 7.13043C11.05 6.93168 10.95 6.73292 10.8 6.43478L11.35 5.68944L10.3 4.64596L9.55 5.19255C9.3 5.04348 9.05 4.9441 8.85 4.89441L8.75 4H7.3L7.15 4.89441C6.9 4.9441 6.7 5.04348 6.45 5.19255L5.7 4.64596L4.65 5.68944L5.15 6.43478C5 6.68323 4.95 6.88199 4.85 7.13043L4 7.22981V8.67081L4.9 8.81988C4.95 9.06832 5.05 9.26708 5.2 9.51553L4.7 10.2609L5.75 11.3043L6.5 10.8075C6.7 10.9068 6.95 11.0062 7.2 11.1056L7.35 12H8.85L9 11.1056C9.25 11.0559 9.45 10.9565 9.7 10.8075L10.45 11.354L11.5 10.3106L10.95 9.56522C11.1 9.31677 11.2 9.06832 11.25 8.86957L12 8.7205ZM8 9.46584C7.15 9.46584 6.5 8.81988 6.5 7.97516C6.5 7.13043 7.15 6.48447 8 6.48447C8.85 6.48447 9.5 7.13043 9.5 7.97516C9.5 8.81988 8.85 9.46584 8 9.46584Z" fill="#666666" />
-    </svg>
+    icon: CalendarIcon
   },
 };
 
--- libs/ui/src/lib/Badge/Badge.tsx
+++ libs/ui/src/lib/Badge/Badge.tsx
@@ -1,9 +1,63 @@
 import { VariantProps, cva } from "class-variance-authority";
 import { ElementRef, forwardRef } from "react";
 import { cn } from "../Button/Button";
+import { CalendarIcon } from '@heroicons/react/24/solid'
 
-const badge = cva(['transition-all rounded-xl flex items-center gap-1 flex-start text-neutral-700 bg-neutral-100 w-max'], {
+const badge = cva([
+  'transition-all duration-200 rounded-xl flex items-center gap-1 flex-start text-neutral-700 bg-neutral-100 w-max',
+  'active:ring-[3px] focus:ring-[3px] ring-ui-blue-500 ring-offset-2 active:outline-none cursor-pointer',
+], {
   variants: {
+    intent: {
+      'neutral-muted': [
+        'bg-neutral-50',
+        'text-neutral-600',
+        'hover:outline outline-1 outline-neutral-200',
+        'active:text-neutral-800 active:bg-neutral-100'
+      ],
+      'neutral': [
+        'bg-neutral-100',
+        'text-neutral-700',
+        'hover:outline outline-1 outline-neutral-200 hover:text-neutral-800',
+        'focus:text-neutral-800',
+        'active:text-neutral-900 active:bg-neutral-200'
+      ],
+      'info': [
+        'bg-ui-blue-100',
+        'text-ui-blue-700',
+        'hover:outline outline-1 outline-ui-blue-200 hover:text-ui-blue-800',
+        'focus:text-ui-blue-800',
+        'active:text-ui-blue-900 active:bg-ui-blue-200'
+      ],
+      'success': [
+        'bg-ui-green-100',
+        'text-ui-green-700',
+        'hover:outline outline-1 outline-ui-green-200 hover:text-ui-green-800',
+        'focus:text-ui-green-800',
+        'active:text-ui-green-900 active:bg-ui-green-200'
+      ],
+      'warning': [
+        'bg-ui-orange-100',
+        'text-ui-orange-700',
+        'hover:outline outline-1 outline-ui-orange-200 hover:text-ui-orange-800',
+        'focus:text-ui-orange-800',
+        'active:text-ui-orange-900 active:bg-ui-orange-200'
+      ],
+      'danger': [
+        'bg-ui-red-100',
+        'text-ui-red-700',
+        'hover:outline outline-1 outline-ui-red-200 hover:text-ui-red-800',
+        'focus:text-ui-red-800',
+        'active:text-ui-red-900 active:bg-ui-red-200'
+      ],
+      'tier': [
+        'bg-ui-purple-100',
+        'text-ui-purple-700',
+        'hover:outline outline-1 outline-ui-purple-200 hover:text-ui-purple-800',
+        'focus:text-ui-purple-800',
+        'active:text-ui-purple-900 active:bg-ui-purple-200'
+      ],
+    },
     size: {
       'SM': ['pr-2 text-xs rounded-lg'],
       'MD': ['p-1 pr-2 text-xs rounded-xl'],
@@ -11,19 +65,20 @@ const badge = cva(['transition-all rounded-xl flex items-center gap-1 flex-start
     }
   },
   defaultVariants: {
-    size: 'SM'
+    size: 'SM',
+    intent: 'neutral-muted'
   }
 })
 
 export interface BadgeProps
   extends VariantProps<typeof badge> {
-  icon?: React.ReactNode
+  icon: typeof CalendarIcon
   label?: string | React.ReactNode
 }
-const Badge: React.FC<BadgeProps> = forwardRef<ElementRef<'div'>, BadgeProps>(({ icon, size, label, ...props }, ref) => {
+const Badge: React.FC<BadgeProps> = forwardRef<ElementRef<'div'>, BadgeProps>(({ intent, icon: Icon, size, label, ...props }, ref) => {
   return (
-    <div ref={ref} className={cn(badge({ size }))} {...props}>
-      {icon}
+    <div ref={ref} className={cn(badge({ intent, size }))} {...props}>
+      <Icon className="w-4 h-4" />
       {label}
     </div>
   );
