f6b1b5619ecb967aab3f2384e4e0f084b8d95793
John Ardosa <jardosa@live.com>
2023-11-05 14:46:02 +0900

    feat: update breadcrumbs

--- libs/ui/src/lib/Breadcrumbs/Breadcrumbs.stories.tsx
+++ libs/ui/src/lib/Breadcrumbs/Breadcrumbs.stories.tsx
@@ -1,8 +1,42 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Breadcrumbs } from './Breadcrumbs';
+import { AiFillGitlab } from 'react-icons/ai/index'
+import Breadcrumbs, { Breadcrumb } from './Breadcrumbs';
 
 import { within } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
+
+const items: Breadcrumb[] = [
+  {
+    label: 'First Item',
+    src: '/',
+    avatar: AiFillGitlab
+  },
+  {
+    label: 'Second Item',
+    src: '/',
+    avatar: AiFillGitlab
+  },
+  {
+    label: 'Third Item',
+    src: '/',
+    avatar: AiFillGitlab
+  },
+  {
+    label: 'Fourth Item',
+    src: '/',
+    avatar: AiFillGitlab
+  },
+  {
+    label: 'Fifth Item',
+    src: '/',
+    avatar: AiFillGitlab
+  },
+  {
+    label: 'Sixth Item',
+    src: '/',
+    avatar: AiFillGitlab
+  },
+]
 
 const meta: Meta<typeof Breadcrumbs> = {
   component: Breadcrumbs,
@@ -12,7 +46,9 @@ export default meta;
 type Story = StoryObj<typeof Breadcrumbs>;
 
 export const Primary = {
-  args: {},
+  args: {
+    items
+  },
 };
 
 export const Heading: Story = {
--- libs/ui/src/lib/Breadcrumbs/Breadcrumbs.tsx
+++ libs/ui/src/lib/Breadcrumbs/Breadcrumbs.tsx
@@ -1,10 +1,56 @@
-/* eslint-disable-next-line */
-export interface BreadcrumbsProps {}
+import Link from "next/link";
+import { FC, useMemo, useState } from "react";
+import { IconType } from "react-icons/lib";
+import { AiOutlineEllipsis } from 'react-icons/ai/index'
 
-export function Breadcrumbs(props: BreadcrumbsProps) {
+export type Breadcrumb = {
+  avatar?: IconType;
+  label?: string;
+  src?: string;
+  onClick?: () => void
+};
+
+const Breadcrumb: FC<Breadcrumb> = ({ avatar: Avatar, label, src, onClick }) => {
+  return <div className="flex items-center gap-1 group last:font-bold text-neutral-700">
+    {Avatar && <Avatar
+      {...onClick && { role: 'button', onClick: () => onClick() }}
+      className="w-4 h-4 rounded-[4px] border" />}
+    {!!src && <Link href={src}>{label}</Link>}
+    <div className="group-last:hidden">/</div>
+  </div>
+}
+
+export type BreadcrumbsProps = {
+  items: Breadcrumb[]
+}
+
+
+const Breadcrumbs: FC<BreadcrumbsProps> = ({ items }) => {
+  const [expandItems, setExpandItems] = useState(false)
+
+  const splicedItems = useMemo(() => {
+    if (items.length > 3 && !expandItems) {
+      return [...items.slice(0, 1),
+      {
+        avatar: AiOutlineEllipsis,
+        onClick: () => {
+          setExpandItems(true)
+        }
+      },
+      ...items.slice(-2),]
+    }
+    return items
+  }, [items, expandItems])
+
   return (
-    <div>
-      <h1>Welcome to Breadcrumbs!</h1>
+    <div className="flex items-center gap-2">
+      {!!splicedItems?.length && splicedItems?.map((value) => <Breadcrumb
+        key={JSON.stringify(value)}
+        avatar={value.avatar}
+        label={value.label}
+        src={value.src}
+        onClick={value.onClick}
+      />)}
     </div>
   );
 }
